2f031b4b6b5875234ef9be68ee04b691fd8f6813
v0idZec <v0idZec42@gmail.com>
2025-09-18 23:53:38 +0200

    first commit

---
+++ pwdGen.py
@@ -0,0 +1,18 @@
+import secrets
+import string
+import random
+
+# TODO: improve
+
+def genPass(length = 42, letters = True, digits = True, extras = True) -> str:
+    assert length >= 16
+    assert sum([letters, digits, extras]) >= 2
+
+    all = list((string.ascii_letters if letters else '') + (string.digits if digits else '') + (string.punctuation if extras else ''))
+
+    pwd = ""
+    for _ in range(length):
+        random.shuffle(all)
+        pwd += secrets.choice(all)
+
+    return pwd

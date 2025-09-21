c73be7c603a96db986d5ac4d9c610addb8e87466
v0idZec <v0idZec42@gmail.com>
2025-09-21 19:52:30 +0200

    bc it's python

--- binaryToDecimal.py
+++ binaryToDecimal.py
@@ -1,8 +1,10 @@
-def binToDec(binary: int) -> int:
-    c: int = 1;
-    decimal: int = 0
+def binToDec(binary) -> int:
+    decimal = 0
+    c = 1;
+
     for n in reversed(str(binary)):
         if n == '1':
             decimal += c
         c *= 2
+        
     return decimal
--- decimalToBinary.py
+++ decimalToBinary.py
@@ -1,6 +1,7 @@
-def decToBin(dec: int) -> int:
-    binary: str = ""
+def decToBin(dec) -> int:
+    binary = ""
     while dec > 0:
         binary += str(dec % 2)
         dec //= 2;
+    
     return int(binary[::-1])
